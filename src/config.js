c8c3e2d4b8128cdc03a052dd5e52e72011806658
root <root@DESKTOP-O8ESL65.localdomain>
2021-01-20 16:11:47 -0500

    Building React Client App

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 const config = {
+    STRIPE_KEY: "pk_test_51IBgYSHjTUCGW8LImNYecw6E41nMjmaqwucfHvK0Ysqeha88cQqPTJhHuoMRLHdlX6n8hCFxNazUPAiMpB3hlqIi00VJ7kPc6S",
+    MAX_ATTACHMENT_SIZE: 5000000,
     s3: {
         REGION: "us-east-2",
         BUCKET: "notes-app-uploads-001",
@@ -11,7 +13,7 @@ const config = {
         REGION: "us-east-2",
         USER_POOL_ID: "us-east-2_6j05mJPFP",
         APP_CLIENT_ID: "7dpifjb7qmm2r3q97d6nc324ic",
-        IDENTITY_POOL_ID: "us-east-2:6f0ca6df-a360-4127-a40f-a55b4698b9e0",
+        IDENTITY_POOL_ID: "us-east-2:0ecbbb38-07dd-48e2-aece-227df5604199",
     },
 };
 export default config;
